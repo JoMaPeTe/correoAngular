11a8cb250ec9ddeadde88b61ef94a1783658e3f8
JoseMPeTe <jmperezterrero@gmail.com>
2020-10-26 21:14:59 +0100

    Añadimos el modulo necesario y creamos un componente vacio para realizar un formulario simple para enviar un correo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './Components/correo/correo.component';
 import { ListaCorreoComponent } from './Components/lista-correo/lista-correo.component';
+import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CorreoComponent,
-    ListaCorreoComponent
+    ListaCorreoComponent,
+    NuevoCorreoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
