602b4fff2fe86b3442bc16c558925f557b2564f4
JoseMPeTe <jmperezterrero@gmail.com>
2020-10-26 16:50:37 +0100

    Incluimos otro ngIf para mostrar dos estados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './Components/correo/correo.component';
+import { ListaCorreoComponent } from './Components/lista-correo/lista-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CorreoComponent
+    CorreoComponent,
+    ListaCorreoComponent
   ],
   imports: [
     BrowserModule,
