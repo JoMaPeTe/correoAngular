4635f7e49c993e216cf8c2ace87d0731682f5838
JoseM <josmanpeter77@gmail.com>
2020-10-27 17:25:56 +0100

    Colocamos en app.component el componente lista para verlo mientras lo editamos

--- src/app/Components/lista-correo/lista-correo.component.ts
+++ src/app/Components/lista-correo/lista-correo.component.ts
@@ -3,16 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lista-correo',
   templateUrl: './lista-correo.component.html',
-  styleUrls: ['./lista-correo.component.scss']
+  styleUrls: ['./lista-correo.component.scss'],
 })
 export class ListaCorreoComponent implements OnInit {
-
- 
   correos: any[];
 
   constructor() {
     const correo1 = {
-      titulo: "Titulo del 1",
+      titulo: 'Titulo del 1',
       cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
         Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email,
         Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email`,
@@ -21,7 +19,7 @@ export class ListaCorreoComponent implements OnInit {
       leido: true,
     };
     const correo2 = {
-      titulo: "Titulo del 2",
+      titulo: 'Titulo del 2',
       cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
         Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
       emisor: 'correoEmisor2@openWebinar.inv',
@@ -31,10 +29,24 @@ export class ListaCorreoComponent implements OnInit {
     this.correos = [];
     this.correos.push(correo1);
     this.correos.push(correo2);
+
+    this.correos.push({
+      titulo: 'Titulo del 3',
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor3@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    });
+    this.correos.push({
+      titulo: 'Titulo del 4',
+      cuerpo: `Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email
+        Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuer`,
+      emisor: 'correoEmisor4@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv',
+      leido: false,
+    });
   }
 
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
